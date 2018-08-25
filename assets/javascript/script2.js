17159b6c19d8179a9350e3dd1a19d0e1f3491e66
Ha1f_Deaf <jaredtspears@gmail.com>
2018-08-25 18:08:14 -0400

    styled a bit more because my js is not working perfectly yet. Will be continuing to work on the submit button display.

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -1,6 +1,7 @@
 
 //question array, the answer array, and the incorrect answer array
 //set up with radio buttons after the object Timer.
+
 var qArr = ["1. Who is the father of western philosophy?", "2. Which philosopher said I think therefore I am?", 
 "3. What is Platos theory of forms talk about?", "4. Who was the father of modernism?","5. What was Socretes known for?", 
 "6. Kant wrote which of these books?", "7. Which philosopher came up with deconstructionism?", "8. Hegel wrote which of these books?", 
@@ -11,17 +12,18 @@ var qArr = ["1. Who is the father of western philosophy?", "2. Which philosopher
 "15.Who wrote Fear and Trembling andthe famous consept of the leap of faith.", 
 "16. Socialism was invented by.","17. Foucalt was known for what book?", "18. Lyotard was known for book in postmodernism?", 
 "19. Machiavelli did what for philosophy?", "20. Spinoza did what famous contribution to philosophy?"];
-var aArr = ["Plato ", "Descartes ", "That invisible forms represent the most true ideal expression of reality. ", "Descartes ",
-"He is credited with laying the fundamentals of modern Western philosophy. He is known for creating Socratic irony and the Socratic method. ", 
-"Religion Within the Limits of Reason Alone", "Derrida", "Time and Being", "high school freshman ", "The awoken man ", "Pascal ", "Darwin ", "Freud ", 
-"Nietzche ", "Keirkegaard ", "Marx ", " Discipline and Punish ", "The Postmodern Condition: A Report on Knowledge ", 
-"Being regarded as the father of modern political science ", 
-"He was best known for his Ethics, a monumental work that presents an ethical vision unfolding out of a monistic metaphysics in which God and Nature are identified. "];
-var iArr = ["Socretes", "Hume ", "Forms are real and matter ethically. ", "Hobbs ",
-"He was Plato's student. ", "Book of Reasonable Philosophy ", "Paine ", "Between Time ", 
-"Graduate Students ", "The asleep man ", "Bacon ", "Dalton ", "Dewey ", "Plato ", "Bergson ",
-"Keirkegaard ", " Crime and Politics ", "The Modern Condition: A Report on Reason ", 
-"Being regarded as the father of modern social science", "Best known for the theory on Being "];
+//reminder for the future place spaces before and after each so it displays better with the radio buttons
+var aArr = [" Plato ", " Descartes ", " That invisible forms represent the most true ideal expression of reality. ", " Descartes ",
+" He is credited with laying the fundamentals of modern Western philosophy. He is known for creating Socratic irony and the Socratic method. ", 
+" Religion Within the Limits of Reason Alone", " Derrida", "Time and Being", "high school freshman ", " The awoken man ", " Pascal ", " Darwin ", " Freud ", 
+" Nietzche ", " Keirkegaard ", " Marx ", " Discipline and Punish ", " The Postmodern Condition: A Report on Knowledge ", 
+" Being regarded as the father of modern political science ", 
+" He was best known for his Ethics, a monumental work that presents an ethical vision unfolding out of a monistic metaphysics in which God and Nature are identified. "];
+var iArr = [" Socretes", " Hume ", " Forms are real and matter ethically. ", " Hobbs ",
+" He was Plato's student. ", " Book of Reasonable Philosophy ", " Paine ", " Between Time ", 
+" Graduate Students ", " The asleep man ", " Bacon ", " Dalton ", " Dewey ", " Plato ", " Bergson ",
+" Keirkegaard ", " Crime and Politics ", " The Modern Condition: A Report on Reason ", 
+" Being regarded as the father of modern social science ", " Best known for the theory on Being "];
 
 //global vars
 var correctAnswer = 0;
@@ -46,27 +48,31 @@ var Timer = {
         time =0;
         $("#display").text("00:00");
         clearInterval(intervalId);
-        TimerRunning = false;
-        console.log(intervalId);
-       //tring to make it so the counter does not continue past 0
-       if(Timer.time === -1){
-        clearInterval(intervalId);
-    }
+        timerRunning = false;
     },
     start: function() {
         //if the timer is not running do this if statement
         if(!timerRunning){
             intervalId = setInterval(Timer.count, 1000);
-            clockRunning = true;
+            timerRunning = true;
         }
      
     },
     count: function (){
-        //should desend in time
-        Timer.time --;
-        var converted = Timer.timeConverter(Timer.time);
-            //trying to display what is in the console.log but it is not currently
-        $("#displayCountDown").html("<h2>Time Remaining: " + converted + "</h2>");
+
+        if(timerRunning){
+            //should desend in time
+            Timer.time --;
+            var converted = Timer.timeConverter(Timer.time);
+             //trying to display what is in the console.log but it is not currently
+            $("#displayCountDown").html("<h2>Time Remaining: " + converted + "</h2>");
+        }
+       
+        //tring to make it so the counter does not continue past 0
+        if(Timer.time === 0){
+            timerRunning = false;
+        }
+   
     },
     timeConverter: function(t) {
         var minutes = Math.floor(t/60);
@@ -90,20 +96,24 @@ function gameStart(){
     //loop to go through the questions 
     for(var i=0; i < qArr.length; i++ ){
     var theForm =  $("<form>");
-    //declaring the questions to a paragraph with the text being read from the array in order of the array. 
-    var theQ = $("<p>").text(qArr[i]);
-    //be keen to note the name is needed so only 1 answer will allow for per <p> added <br> to make them line up vertically
+    //declaring the questions to a paragraph with the text being read from 
+    //the array in order of the array. 
+    var theQ = $("<br><p>").text(qArr[i]);
+    //be keen to note the name is needed so only 1 answer will allow for per <p> 
+    //added <br> to make them line up vertically
     var theA = $("<br><input type='radio' name='answerGroup"+i+"' value = 'correct'>"); 
     var theI = $("<br><input type='radio' name='answerGroup"+i+"' value = 'incorrect'>");
 
     //appending the form with the questions
     theForm.append(theQ);
     
-        //appending the form with the correct answers for the radio button, then appending the aArr with index of i 
+        //appending the form with the correct answers for the radio button, 
+        //then appending the aArr with index of i 
          theForm.append(theA);
         theForm.append(aArr[i]);
 
-        //appending the form with the incorrect answers for the radio button, then appending the iArr with index of i 
+        //appending the form with the incorrect answers for the radio button,
+        // then appending the iArr with index of i 
         theForm.append(theI);
         theForm.append(iArr[i])
 
@@ -112,4 +122,34 @@ function gameStart(){
     //not sure if I should do a if else statement
    
     }
+
+    practice()
+    function practice(){
+        $(".test").text("Testing application")
+    }
+
+
+
+    //function to randomize the aArr and iArr
+    randomizer();
+    function randomizer(){
+        rando =[theA, theI];
+    var random = Math.floor(Math.random(rando))
+   
+    }  
+
+    //adding up the correct answer
+    correctAnswer();
+    function correctAnswer(){
+        var user;
+        //if user picks the correct button answer
+        if (user === aArr[i]){
+            //trying to get this to submit the calculated answers 
+            theA = $("<br><input name='Submit1' type='submit' value= 'submit' onclick='Test(this.form)'>")
+            correctAnswer ++;
+            $("#returnScore").html("Pretend I have a working game and you won! ")
+        }
+    }
 }
+
+/* attempted several ways to get teh function of randomizer to work... not working. */
